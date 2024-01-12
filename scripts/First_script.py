b6932c84210aa996c3a5ea7e31e5868cc9e5d54a
SandroTalavera <sandrotalaveraruiz@gmail.com>
2024-01-12 15:35:19 +0100

    first steps

---
+++ scripts/First_script.py
@@ -0,0 +1,15 @@
+"""
+Script to introduce scripts
+"""
+import pandas as pd
+
+def main():
+    """
+    main function to perform a given task
+    """
+    print('hello I am in the main function')
+
+
+if __name__ == '__main__':
+    print('hello I come here first')
+    main()
